d4c32fd727ed76dfabd53434086a20eac1129314
DeTorres <detorrez@yandex.ru>
2024-08-03 12:51:54 +0400

    initial commit - generated from Spring Initializr application

---
+++ src/main/java/cmstricks/ru/TodoistTaskDistributor/TodoistTaskDistributorApplication.java
@@ -0,0 +1,13 @@
+package cmstricks.ru.TodoistTaskDistributor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoistTaskDistributorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoistTaskDistributorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cmstricks/ru/TodoistTaskDistributor/TodoistTaskDistributorApplicationTests.java
@@ -0,0 +1,13 @@
+package cmstricks.ru.TodoistTaskDistributor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoistTaskDistributorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
